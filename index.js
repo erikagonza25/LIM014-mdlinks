1058410042331dd5fc9aecc8b8aead0c59019d0f
Erika <erikagonza2013@gmail.com>
2021-03-16 09:28:31 -0500

    Creación de funciones para la API

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+const process = require('process');
+const path = require('path');
+// Constante para obtener el directorio actual
+const directorio = process.cwd();
+// Constante con un objeto vacio, para llevarlo a los test
+const mdLinks = {};
+// Función para cambiar una ruta relativa a absoluta
+const changeDirectory = (dir) => {
+  try {
+    return path.resolve(dir);
+  } catch (err) {
+    return ('Se produjo un error mientras se cambiaba de directorio ' + err);
+  }
 };
+changeDirectory(directorio);
+// Función para saber si es un archivo o un directorio
+const methods = ['isDirectory', 'isFile'];
+const contentFiles = fs.readdirSync(__dirname, { withFileTypes: true }).map((isDirectory) => {
+  const archivos = { name: isDirectory.name + path.extname(__filename) };
+  for (const value of methods) archivos[value] = isDirectory[value]();
+  return archivos;
+});
+console.table(contentFiles);
+
+mdLinks.changeDirectory = changeDirectory;
+// mdLinks.fileOrDirectory = fileOrDirectory;
+module.exports = mdLinks;
