23817ee6a1d5b7f666b9cb7b5f16a8c0eebd2a95
Erika <erikagonza2013@gmail.com>
2021-03-17 10:25:05 -0500

    Test para la primera función convertir ruta relativa a absoluta

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,15 +1,48 @@
-const mdLinks = require('../index.js');
+const mdLinks = require("../index.js");
 
-describe('describe la función checkFileExists', () => {
-  it('cambia la ruta de relativa a absoluta', () => {
-    expect(mdLinks.changeDirectory('wwwroot')).toEqual('C:\\Users\\ERIKA\\LIM014-mdlinks\\wwwroot');
+describe("describe la función checkFileExists", () => {
+  it("is a function", () => {
+    expect(typeof mdLinks.changeDirectory).toBe("function");
+  });
+
+  it("error al cambiar de ruta", () => {
+    expect(mdLinks.changeDirectory()).toBe(
+      'Se produjo un error mientras se cambiaba de directorio TypeError [ERR_INVALID_ARG_TYPE]: The "path" argument must be of type string. Received undefined'
+    );
+  });
+  it("cambia la ruta de relativa a absoluta", () => {
+    expect(mdLinks.changeDirectory("wwwroot")).toEqual(
+      "C:\\Users\\ERIKA\\LIM014-mdlinks\\wwwroot"
+    );
   });
 });
-it('error al cambiar de ruta', () => {
-  expect(mdLinks.changeDirectory()).toBe('Se produjo un error mientras se cambiaba de directorio TypeError [ERR_INVALID_ARG_TYPE]: The "path" argument must be of type string. Received undefined');
+/* describe('describe la función fileOrDirectory', () => {
+  it('', () => {
+    expect(mdLinks.obtenerLinks('.editorconfig.js')).toEqual('isFile');
+  });
+  it('', () => {
+    expect(mdLinks.obtenerLinks('.git.js')).toBe('isDirectory');
+  });
+});
+describe('describe la función prueba', () => {
+  it('', () => {
+    expect(mdLinks.prueba('index.js')).toEqual('index.js');
+  });
+
+  it('', () => {
+    expect(mdLinks.prueba('')).toEqual('El archivo no existe, verifique la información proporcionada');
+  });
+});
+describe('describe la función pruebaTwo', () => {
+  it('', () => {
+    expect(mdLinks.pruebaTwo('erika')).toEqual('El directorio no existe, verifique la información proporcionada');
+  });
+  it('', () => {
+    expect(mdLinks.pruebaTwo('test')).toBe('test');
+  });
 });
 
-/* describe('describe la función checkFileExists', () => {
+ describe('describe la función checkFileExists', () => {
   it('debe devolver la ruta del archivo (./README.md) si el archivo existe', () => {
     expect(mdLinks.fileExists('./README.md')).toEqual('./README.md');
   });
