9f160332e008c5d52ec4b90f23fe4ea67cfa442b
Erika <erikagonza2013@gmail.com>
2021-03-19 13:16:40 -0500

    Agregando dos nuevas funciones(cambiar archivo a HTML, leer archivo y obtener links)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const process = require("process");
 const path = require("path");
+const marked = require("marked");
+const cheerio = require("cheerio");
 // Constante para obtener el directorio actual
 const directorio = process.cwd();
 // Constante con un objeto vacio, para llevarlo a los test
@@ -9,6 +11,9 @@ const mdLinks = {};
 const test = "./prueba";
 // Constante de prueba para la lectura de archivos
 const readFile = "README.md";
+//Constante de prueba para la lectura de archivos y obtener los links
+const fileHtml = "hola.html";
+
 // Función para cambiar una ruta relativa a absoluta
 const changeDirectory = (dir) => {
   try {
@@ -71,6 +76,17 @@ const readMd = (read) => {
   }
 };
 readMd(readFile);
+// Función para cambiar un .md a html
+const changeMdToHtml = (condicion) => marked(condicion);
+console.log(changeMdToHtml(readFile));
+// Función para extraer los links de un archivo html
+const searchLinks = (condicion) => {
+  const readHtml = cheerio.load(fs.readFileSync(condicion, "utf8"));
+  const allLinks = readHtml("a").map((i, el) => readHtml(el).attr("href"));
+  return allLinks;
+};
+searchLinks(fileHtml);
+console.log(searchLinks(fileHtml));
 
 mdLinks.changeDirectory = changeDirectory;
 mdLinks.existFile = existFile;
