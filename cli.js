ebe979c9c3976cb4e0c9672c3bdefe23755ebb78
Erika <erikagonza2013@gmail.com>
2021-04-06 23:55:12 -0500

    Cli listo(tiene los 4 retornos correspondientes)

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const chalk = require("chalk");
 const { mdLinks } = require("./src/index.js");
-const { message } = require("./image/imagenes.js");
+const { message, messageCat, messaCat } = require("./image/imagenes.js");
 const figlet = require("figlet");
 const program = require("commander");
 
@@ -30,13 +30,7 @@ program
   )
 
   .action((path) => {
-    let options = {
-      validate: false,
-      stats: false,
-    };
-
-    options.validate = program.validate;
-    options.stats = program.stats;
+    const options = program.opts();
 
     mdLinks(path, options).then((arrayLinks) => {
       if (!options.validate && !options.stats) {
@@ -48,13 +42,53 @@ program
               "  " +
               chalk.hex("#E3BAC6").dim(link.file)
           );
-          /*console.log(
-            `${chalk.hex("#7D53DE").bold(link.href)}\t ${chalk
-              .hex("#DAFFEF")
-              .dim(link.text)}\t ${chalk.hex("#E3BAC6").dim(link.file)}\t`
-          ); */
         });
         console.log(chalk.hex("#FFCAD4")(message));
+      } else if (options.validate && !options.stats) {
+        arrayLinks.forEach((link) => {
+          console.log(
+            chalk.hex("#7D53DE").bold(link.href) +
+              "  " +
+              chalk.hex("#DAFFEF").dim(link.text) +
+              "  " +
+              chalk.hex("#E3BAC6").dim(link.file) +
+              "  " +
+              chalk.hex("#31AFD4").dim(link.status) +
+              "  " +
+              chalk.hex("#F7CB15").dim(link.statusText)
+          );
+        });
+        console.log(chalk.hex("#FFCAD4")(messageCat));
+      } else if (!options.validate && options.stats) {
+        [arrayLinks].forEach((link) => {
+          console.log(
+            chalk.hex("#7D53DE").bold("Total:") +
+              " " +
+              chalk.hex("#DAFFEF").dim(link.Total) +
+              "\n" +
+              chalk.hex("#7D53DE").bold("Unique:") +
+              " " +
+              chalk.hex("#DAFFEF").dim(link.Unique)
+          );
+        });
+        console.log(chalk.hex("#FFCAD4")(messaCat));
+      } else if (options.validate && options.stats) {
+        [arrayLinks].forEach((link) => {
+          console.log(
+            chalk.hex("#7D53DE").bold("Total:") +
+              " " +
+              chalk.hex("#DAFFEF").dim(link.Total) +
+              "\n" +
+              chalk.hex("#7D53DE").bold("Unique:") +
+              " " +
+              chalk.hex("#DAFFEF").dim(link.Unique) +
+              "\n" +
+              chalk.hex("#7D53DE").bold("Broken:") +
+              " " +
+              chalk.hex("#DAFFEF").dim(link.Broken)
+          );
+        });
+        console.log(chalk.hex("#FFCAD4")(messaCat));
       }
     });
   })
@@ -67,53 +101,8 @@ program
     resolve(msn("MD-LINKS"));
   });
 })();
-
-/*#!/usr/bin/env node
-const chalk = require("chalk");
-const figlet = require("figlet");
-const inquirer = require("inquirer");
-const program = require("commander");
-
-// Mostrar un banner con un mensaje formado por caracteres.
-const msn = (msn) => {
-  console.log(
-    chalk.bold.magenta(
-      figlet.textSync(msn, {
-        font: "Larry 3D",
-        horizontalLayout: "default",
-        verticalLayout: "default",
-      })
-    )
-  );
-};
-//
-
-// Preguntas que se van a realizar y que más tarde usaremos
-const queryParams = () => {
-  const qs = [
-    {
-      name: "componentName",
-      type: "input",
-      message: "Ingrese la ruta",
-    },
-
-    {
-      name: "type",
-      type: "list",
-      message: "Options: ",
-      choices: ["--no options", "--validate", "--stats", "--stats y validate"],
-    },
-  ];
-
-  return inquirer.prompt(qs);
-};
-
-const createFile = (data) => {};
-// IIFE (Immediately Invoked Function Expression)
-(() => {
-  return new Promise((resolve) => {
-    msn("MD-LINKS");
-    createFile(resolve(queryParams()));
-  });
-})();
-*/
+/*console.log(
+            `${chalk.hex("#7D53DE").bold(link.href)}\t ${chalk
+              .hex("#DAFFEF")
+              .dim(link.text)}\t ${chalk.hex("#E3BAC6").dim(link.file)}\t`
+          ); */
