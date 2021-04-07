260cc1af511b59fee7c8fbbf7a82e8a444cc963f
Erika <erikagonza2013@gmail.com>
2021-04-06 20:14:00 -0500

    Imagenes para el cli

---
+++ image/imagenes.js
@@ -0,0 +1,25 @@
+const message = `
+──────▄▀▄─────▄▀▄
+─────▄█░░▀▀▀▀▀░░█▄
+─▄▄──█░░░░░░░░░░░█──▄▄
+█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█
+`;
+const help = `
+                  __
+                     .'  '.
+                 _.-'/  |  \
+    ,        _.-"  ,|  /  0  -.                            HELP!
+    |\    .-"        --""-.__.'======================================================================-,
+    \ '-'         .___.--._)=========================================================================|
+     \            .'      |                             -v, --validate                               |
+      |     /,_.-'        |                   "Valida si el status de los links"                     |
+    _/   _.'(             |                              -s, --stats                                 |
+   /  ,-' \  \            |       "Muestra estadisticas de los links(Total, Unique y Broken)"        |
+   \  \     -'            |                         -s,-v --stats --validate                         |
+    \  \                  |"Devuelve estadísticas que necesiten de los resultados de la validación"  |
+       -'                  '------------------------------------------------------------------------'
+`;
+module.exports = {
+  message,
+  help,
+};
